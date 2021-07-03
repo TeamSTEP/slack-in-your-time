50ca4f5776b78edff7c11aa64187d9ead7f05a8f
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-07-03 13:32:10 +0900

    remove action block

--- src/view/appHomeBlock.ts
+++ src/view/appHomeBlock.ts
@@ -1,9 +1,14 @@
+import { Users } from '../model';
+import moment from 'moment-timezone';
+
 interface HomeBlockProps {
     userId: string;
+    userInfo: Users.User;
 }
 
 export const appHomeBlock = (props: HomeBlockProps) => {
-    const today = new Date();
+    const timezone = props.userInfo.tz_label;
+    const time = moment(timezone);
     return [
         {
             type: 'header',
@@ -20,114 +25,8 @@ export const appHomeBlock = (props: HomeBlockProps) => {
             type: 'section',
             text: {
                 type: 'mrkdwn',
-                text: `Local time of <@${props.userId}>: ${today.toString()}`,
+                text: `Local time <@${props.userId}>: ${time.toString()}`,
             },
-        },
-        {
-            type: 'input',
-            element: {
-                type: 'static_select',
-                placeholder: {
-                    type: 'plain_text',
-                    text: 'Select an item',
-                    emoji: true,
-                },
-                options: [
-                    {
-                        text: {
-                            type: 'plain_text',
-                            text: 'JST (UTC+9:00)',
-                            emoji: true,
-                        },
-                        value: 'value-0',
-                    },
-                    {
-                        text: {
-                            type: 'plain_text',
-                            text: 'MST (UTC-07:00)',
-                            emoji: true,
-                        },
-                        value: 'value-1',
-                    },
-                    {
-                        text: {
-                            type: 'plain_text',
-                            text: 'AFT (UTC+04:30)',
-                            emoji: true,
-                        },
-                        value: 'value-2',
-                    },
-                ],
-                action_id: 'static_select-action',
-            },
-            label: {
-                type: 'plain_text',
-                text: 'Sender time zone',
-                emoji: true,
-            },
-        },
-        {
-            type: 'input',
-            element: {
-                type: 'plain_text_input',
-                multiline: true,
-                action_id: 'plain_text_input-action',
-            },
-            label: {
-                type: 'plain_text',
-                text: 'Message Input',
-                emoji: true,
-            },
-        },
-        {
-            type: 'actions',
-            elements: [
-                {
-                    type: 'button',
-                    text: {
-                        type: 'plain_text',
-                        text: 'Parse Time',
-                        emoji: true,
-                    },
-                    value: 'click_me_123',
-                    style: 'primary',
-                    action_id: 'actionId-0',
-                },
-            ],
-        },
-        {
-            type: 'section',
-            text: {
-                type: 'mrkdwn',
-                text: '*Converted time*:',
-            },
-            fields: [
-                {
-                    type: 'plain_text',
-                    text: '*this is plain_text text*',
-                    emoji: true,
-                },
-                {
-                    type: 'plain_text',
-                    text: '*this is plain_text text*',
-                    emoji: true,
-                },
-                {
-                    type: 'plain_text',
-                    text: '*this is plain_text text*',
-                    emoji: true,
-                },
-                {
-                    type: 'plain_text',
-                    text: '*this is plain_text text*',
-                    emoji: true,
-                },
-                {
-                    type: 'plain_text',
-                    text: '*this is plain_text text*',
-                    emoji: true,
-                },
-            ],
         },
     ];
 };
